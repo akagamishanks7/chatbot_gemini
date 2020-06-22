d4b7b14e54899da8c0d7e8d152bab5e8dcd67b5d
shashank <shekhar.shashank764@gmail,com>
2020-06-22 20:48:30 +0530

    created activities for user and agent home page Database on Xampp created

---
+++ app/src/main/java/com/example/chatbot_gemini/AgentHome.java
@@ -0,0 +1,14 @@
+package com.example.chatbot_gemini;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgentHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agent_home);
+    }
+}
